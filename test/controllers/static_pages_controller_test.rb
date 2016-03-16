36efff8fbf6a3d41bd72d156c2f8ea75457153bb
tatyana_pol <tatyana_pol@yahoo.com>
2016-03-16 07:04:29 +0000

    commit

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,28 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Creative App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Creative App"
   end
 
-   test "should get about" do
-    get :about 
+  test "should get about" do
+    get :about
     assert_response :success
-   end
+    assert_select "title", "About | Creative App"
   end
+
+   test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Creative App"
+  end
+end
+
+
+
 
