275ccdb06ca06a3e1a70d2ef74a913dc0c4c4b46
tatyana_pol <tatyana_pol@yahoo.com>
2016-03-16 07:52:29 +0000

    com

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'users/new'
 
+  get 'users/new'
+
   get 'user/new'
 
   root           'static_pages#home'
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
 
   get  'about'   => 'static_pages#about'
   get  'contact' => 'static_pages#contact'
-  get  'signup'  -> 'users#new'
+  get  'signup'  => 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
