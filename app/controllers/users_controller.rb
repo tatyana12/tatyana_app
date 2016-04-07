c3d00f07fcdee73189db9b73b63f0dcb6731c1f9
tatyana_pol <tatyana_pol@yahoo.com>
2016-04-07 16:36:58 +0000

    M

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-before_action :logged_in_user, only: [:edit, :update]
+before_action :logged_in_user, only: [:index, :edit, :update]
 before_action :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
@@ -30,6 +30,10 @@ before_action :correct_user,   only: [:edit, :update]
     else
       render 'edit'
     end
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
   end
   
 private
