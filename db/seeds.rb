4dbd674fa67aa74c3e11bf3b47bd63cbb0879ee2
tatyana_pol <tatyana_pol@yahoo.com>
2016-04-13 02:50:42 +0000

    M

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name:  "Tatyana Polyakova",
-             email: "tatyana_pol@yahoo.com",
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true)
