217783632c0f9d133feae77cff7b4bed0836a8e7
tatyana_pol <tatyana_pol@yahoo.com>
2016-04-14 05:52:46 +0000

    M

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411010748) do
+ActiveRecord::Schema.define(version: 20160414031937) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.string   "activation_digest"
+    t.boolean  "activated"
+    t.datetime "activated_at"
   end
 
 end
