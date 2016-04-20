87091526c29037eeb4f34c99572820abe9767004
tatyana_pol <tatyana_pol@yahoo.com>
2016-04-20 05:39:30 +0000

    add passw and email config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
   class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
+
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -42,7 +43,8 @@
   end  
   
   
-  # Activates an account.
+  
+ # Activates an account.
   def activate
     update_attribute(:activated,    true)
     update_attribute(:activated_at, Time.zone.now)
@@ -53,10 +55,24 @@
     UserMailer.account_activation(self).deliver_now
   end
 
-  
-  
-  
- private
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+private
 
     # Converts email to all lower-case.
     def downcase_email
@@ -69,3 +85,5 @@
       self.activation_digest = User.digest(activation_token)
     end
   end
+
+ 
