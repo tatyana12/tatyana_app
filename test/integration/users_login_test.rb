64de1771563393a54b01be0f11721f8457d37b35
tatyana_pol <tatyana_pol@yahoo.com>
2016-05-10 22:26:41 +0000

    M

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:tatyana)
+    @user = users(:michael)
   end
   
  test "login with valid information" do
