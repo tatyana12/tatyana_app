0478995fe5de5d142a2a9e44970e2bc682e7ea7d
tatyana_pol <tatyana_pol@yahoo.com>
2016-05-11 15:43:39 +0000

    M

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'tatyanaheroku.herokuapp.com'
+  host = 'tatyanaapp.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
