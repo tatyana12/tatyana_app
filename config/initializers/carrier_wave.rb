b1edaabad786a9aea3d5930b1e921680c750877e
tatyana_pol <tatyana_pol@yahoo.com>
2016-05-14 20:14:29 +0000

    M

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,14 +1,12 @@
- if Rails.env.production?
-        CarrierWave.configure do |config|
-        config.root = Rails.root.join('tmp')
-        config.cache_dir = 'carrierwave'
-
-        config.fog_credentials = {
-          # Configuration for Amazon S3
-          :provider               => 'AWS',                        # change var's name
-          :aws_access_key_id      => ENV['S3_ACCESS_KEY'],         # change var's name
-          :aws_secret_access_key  => ENV['S3_SECRETE_KEY']
-        }
-        config.fog_directory     =  ENV['S3_BUCKET']
-      end
-    end
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region                => ENV['S3_REGION']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
+end
