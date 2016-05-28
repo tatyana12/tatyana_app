fe16382851ec8fbd9f151ee38da86384309d2133
tatyana_pol <tatyana_pol@yahoo.com>
2016-05-28 22:25:26 +0000

    M

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512180853) do
+ActiveRecord::Schema.define(version: 20160528183049) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160512180853) do
     t.string   "activation_digest"
     t.boolean  "activated"
     t.datetime "activated_at"
+    t.boolean  "banned"
   end
 
 end
