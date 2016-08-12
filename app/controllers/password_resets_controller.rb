2bdfd69fabc5d3dc46e57d7d5c5a97b783ce3323
tatyana_pol <tatyana_pol@yahoo.com>
2016-08-12 04:03:04 +0000

    M

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -8,7 +8,7 @@ class PasswordResetsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
-    if @user
+    if @user  &&  @user.banned?
       @user.create_reset_digest
       @user.send_password_reset_email
       flash[:info] = "Email sent with password reset instructions"
