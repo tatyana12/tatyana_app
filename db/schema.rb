0f2502451c09183380f2f68fdf28afbf44951921
tatyana_pol <tatyana_pol@yahoo.com>
2016-11-01 19:04:09 +0000

    M

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -19,9 +18,8 @@ ActiveRecord::Schema.define(version: 20160528183049) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "picture"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
-
-  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
